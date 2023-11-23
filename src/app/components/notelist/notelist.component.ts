42fdf220c8acbf7199c6bf3dd242ca1bd7a5921d
Siva Prakash <prakashdeveloper@outlook.in>
2023-11-23 09:04:17 +0530

    Initial commit

---
+++ src/app/components/notelist/notelist.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-notelist',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './notelist.component.html',
+  styles: ``,
+})
+export class NotelistComponent {}
